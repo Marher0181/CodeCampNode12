0497955f3e5ca83be81edc29cb450536e0a36852
Marher0181 <marlonher0181@gmail.com>
2024-12-23 00:50:36 -0600

    modificar orden

--- routes/OrdenesRoutes.js
+++ routes/OrdenesRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const sequelize = require('../db/db');
 const router = express.Router();
+const authenticateAndAuthorizeStr = require('../middlewares/auth');
 
 router.post('/add', async (req, res) => {
     const {
@@ -55,5 +56,57 @@ router.post('/add', async (req, res) => {
         res.status(500).json({ message: 'Error al ingresar orden: ', err });
     }
 });
+
+router.put('/edit/:id', authenticateAndAuthorizeStr("Administrador"), async (req, res) => {
+    const { id } = req.params;
+    const { 
+        usuarios_idUsuario,
+        estados_idEstado,
+        nombre_completo,
+        direccion,
+        telefono,
+        correo_electronico,
+        fecha_entrega,
+        total_orden 
+    } = req.body;
+
+    try {
+        const result = await sequelize.query(
+            'EXECUTE [dbo].[sp_editarOrdenes] ' +
+            '@idOrden = :id, ' +
+            '@usuarios_idUsuario = :usuarios_idUsuario, ' +
+            '@estados_idEstado = :estados_idEstado, ' +
+            '@nombre_completo = :nombre_completo, ' +
+            '@direccion = :direccion, ' +
+            '@telefono = :telefono, ' +
+            '@correo_electronico = :correo_electronico, ' +
+            '@fecha_entrega = :fecha_entrega, ' +
+            '@total_orden = :total_orden', 
+            { 
+                replacements: {
+                    id,
+                    usuarios_idUsuario,
+                    estados_idEstado,
+                    nombre_completo,
+                    direccion,
+                    telefono,
+                    correo_electronico,
+                    fecha_entrega,
+                    total_orden
+                },
+                type: sequelize.QueryTypes.RAW
+            }
+        );
+
+        res.status(200).json({
+            message: 'Orden modificada satisfactoriamente',
+            result
+        });
+    } catch (err) {
+        console.log('Error al modificar la orden: ', err);
+        res.status(500).json({ message: 'Error al modificar la orden: ', error: err.message });
+    }
+});
+
 
 module.exports = router;
